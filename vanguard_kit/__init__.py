8f94d01f76101a3a78c8e82ff6cdddf34301a11e
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2019-10-12 13:22:47 -0500

    Fixed the __init__ file

--- vanguard_kit/__init__.py
+++ vanguard_kit/__init__.py
@@ -1,3 +1,5 @@
 from .vanguard_node import VanguardNode
 from .html_tree import create_html_tree
 from .html_distance import calcuate_html_tree_distance
+
+__all__ = ["VanguardNode", "create_html_tree", "calcuate_html_tree_distance"]
