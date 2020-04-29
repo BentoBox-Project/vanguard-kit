abc6b61b44ccfd6f8f932a7b5096407bc9fae52c
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-04-28 23:01:36 -0500

    build(version): bumping version from 0.1.0 to 0.2.0

--- vanguardkit/__init__.py
+++ vanguardkit/__init__.py
@@ -4,3 +4,6 @@ from .html_tree import create_html_tree
 from .html_distance import calcuate_html_tree_distance
 
 __all__ = ["VanguardNode", "create_html_tree", "calcuate_html_tree_distance"]
+
+
+__version__ = "0.2.0"
